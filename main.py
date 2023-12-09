e6294a8c79e883ba4cde1995184c2bf1468ae752
stepanshustov <step.shustov@mail.ru>
2023-12-09 13:28:47 +0400

    commit 2

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_image(name):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    return image
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    size = width, height = 700, 700
+    screen = pygame.display.set_mode(size)
+    running = True
+    x = y = -1
+    image = load_image("cr.png")
+    image = pygame.transform.scale(image, (100, 100))
+    while running:
+        pygame.mouse.set_visible(False)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION:
+                x, y = event.pos
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        screen.fill((0, 0, 0))
+        if pygame.mouse.get_focused() and x != -1:
+            screen.blit(image, (x, y))
+
+        pygame.display.flip()
